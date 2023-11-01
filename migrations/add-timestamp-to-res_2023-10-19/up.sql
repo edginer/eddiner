a03282fad1e6a177aa2101d0b9c004fb33c0ae69
edge-tainer <edge@example.com>
2023-11-01 18:53:14 +0900

    Delete migration file

--- migrations/add-timestamp-to-res_2023-10-19/up.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE authed_cookies DROP COLUMN last_wrote_time;
-ALTER TABLE responses ADD COLUMN timestamp INTEGER default 0;
-CREATE INDEX responses_authed_token_idx ON responses(authed_token);
