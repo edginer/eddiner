02cbb70f57cd8d1dc104bd6609950211394f41d5
edge-tainer <edge@example.com>
2023-12-14 22:01:54 +0900

    Refactoring migrations

--- migrations/initial.sql
+++ migrations/initial.sql
@@ -1,6 +1,9 @@
 DROP TABLE IF EXISTS threads;
+
 DROP TABLE IF EXISTS boards;
+
 DROP TABLE IF EXISTS responses;
+
 DROP TABLE IF EXISTS authed_cookies;
 
 CREATE TABLE threads (
@@ -17,7 +20,8 @@ CREATE TABLE threads (
 
 CREATE TABLE responses (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT, -- author name
+    name TEXT,
+    -- author name
     mail TEXT,
     date TEXT NOT NULL,
     author_id TEXT,
@@ -25,12 +29,16 @@ CREATE TABLE responses (
     thread_id TEXT NOT NULL,
     ip_addr TEXT NOT NULL,
     authed_token TEXT,
-    timestamp INTEGER DEFAULT 0
+    timestamp INTEGER DEFAULT 0,
+    board_id INTEGER NOT NULL DEFAULT 1,
+    is_abone INTEGER NOT NULL DEFAULT 0
 );
 
 CREATE TABLE boards (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL
+    name TEXT NOT NULL,
+    local_rule TEXT,
+    board_key TEXT
 );
 
 CREATE TABLE authed_cookies (
@@ -52,28 +60,52 @@ CREATE TABLE archives (
     last_modified TEXT NOT NULL
 );
 
-INSERT INTO boards (name) VALUES ('なんでも実況エッヂ');
-INSERT INTO threads (thread_number, title, response_count, last_modified, board_id)
-VALUES ('1696233330', 'test', 1, '1696233330', 1);
-INSERT INTO responses (
-    date,
-    body,
-    thread_id,
-    ip_addr
-) VALUES (
-    'Mon, 02 Oct 2023 07:55:30 GMT',
-    'testtest',
-    '1696233330',
-    '127.0.0.1'
-);
+INSERT INTO
+    boards (name, local_rule, board_key)
+VALUES
+    (
+        'なんでも実況エッヂ',
+        '<hr>
+<br>
+<b>以下がローカルルールです<br><br>
+
+<a href="/">全体の利用規約</a>などはこちらに<br>
+<a href="https://git.3chan.cc/edginer/eddiner/issues">運営・開発への要望・相談・報告はこちらへ</a>
+<br>',
+        'liveedge'
+    );
+
+INSERT INTO
+    threads (
+        thread_number,
+        title,
+        response_count,
+        last_modified,
+        board_id
+    )
+VALUES
+    ('1696233330', 'test', 1, '1696233330', 1);
+
+INSERT INTO
+    responses (date, body, thread_id, ip_addr)
+VALUES
+    (
+        'Mon, 02 Oct 2023 07:55:30 GMT',
+        'testtest',
+        '1696233330',
+        '127.0.0.1'
+    );
 
 CREATE INDEX authed_cookies_cookie_idx ON authed_cookies(cookie);
+
 CREATE INDEX authed_cookies_origin_ip ON authed_cookies(origin_ip);
 
 CREATE UNIQUE INDEX threads_thread_number_idx ON threads(thread_number);
+
 CREATE INDEX threads_authed_token_idx ON responses(authed_token);
 
 CREATE INDEX responses_thread_number_idx ON responses(thread_id);
+
 CREATE INDEX responses_authed_token_idx ON responses(authed_token);
 
 CREATE INDEX archives_thread_number_idx ON archives(thread_number);
